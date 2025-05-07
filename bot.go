d5b22060ed3afad5ba81062aadad06741e6b46c7
Maksim Shahov <utypostev@gmail.com>
2025-05-07 19:48:06 +0300

    very basic tg bot

---
+++ bot.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	botToken := os.Getenv("TELEGRAM_TOKEN") // Или вставь токен напрямую
+	if botToken == "" {
+		log.Panic("INVALID API KEY")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Авторизован как %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Ты написал: "+update.Message.Text)
+			bot.Send(msg)
+		}
+	}
+}
